b09a9b697124e12cfbf39787a96d76bae8be0d52
Lucia Espona Pernas <espona@users.noreply.github.com>
2017-04-20 15:19:27 +0200

    Update travis-build.bash

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -29,7 +29,8 @@ cd -
 
 echo "Installing ckanext-package_converter and its requirements..."
 python setup.py develop
-pip install -r dev-requirements.txt
+pip install -r requirements.txt  --allow-all-external
+pip install -r dev-requirements.txt  --allow-all-external
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
